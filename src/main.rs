4fc84e16163a543cdbf6d340fe8e69308794d0d2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-22 22:42:42 +0530

    [feat]: async main up and running

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
     HeaderValue, Method,
 };
+use db::AppState;
+use std::sync::Arc;
 use tower_http::cors::CorsLayer;
 
 mod db;
+mod handlers;
 mod model;
+mod routes;
 
 #[tokio::main]
 async fn main() {
@@ -14,4 +18,14 @@ async fn main() {
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let pool = db::connect().await;
+    let state = Arc::new(AppState { db: pool });
+    let app = routes::create_router(state).layer(cors);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("🚀 Server started on 0.0.0.0:8000");
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
