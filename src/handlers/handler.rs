d4b28aef7de91ab15a5460dd164052291ce8e868
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-03-22 17:09:58 +0530

    [feat]: added handlers

---
+++ src/handlers/handler.rs
@@ -0,0 +1,8 @@
+use axum::{response::IntoResponse, Json};
+
+pub async fn get_health() -> impl IntoResponse {
+    return Json(serde_json::json!({
+        "status":"200",
+        "message":"Up and Running really fast"
+    }));
+}
